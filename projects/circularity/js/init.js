528bb9ac38805460e660e7cc4fed62906052faa7
Jillian <jillian.r.lovell@gmail.com>
2024-06-24 13:58:35 +0000

    refractor to create 100 circles using loops

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -20,13 +20,22 @@ var init = function (window) {
         ////////////////////////////////////////////////////////////
         
         // TODO 1 : Declare and initialize our variables
-
+        var circle;
+        var circles = [];
 
         // TODO 2 : Create a function that draws a circle 
-        
+        function drawCircle() {
+            // Code to draw a circle
+            circle = draw.randomCircleInArea(canvas, true, true, '#999', 2);
+            physikz.addRandomVelocity(circle, canvas, 10, 10);
+            view.addChild(circle);
+            circles.push(circle);
+        }
 
-        // TODO 3 / 7 : Call the drawCircle() function 
-
+        // TODO 3 / 8 : Call the drawCircle() function 
+        for (var i = 0; i < 50; i++) {
+            drawCircle();
+        }
 
         ////////////////////////////////////////////////////////////
         ///////////////// PROGRAM LOGIC ////////////////////////////
@@ -45,7 +54,10 @@ var init = function (window) {
            
 
             // TODO 9 : Iterate over the array
-           
+            for (var i = 0; i < circles.length; i++) {
+                physikz.updatePosition(circles[i]);
+                game.checkCirclePosition(circles[i]);
+            }
             
         }
     
@@ -61,11 +73,25 @@ var init = function (window) {
                 circle.x = 0;
             }
             
-            // TODO 6 : YOUR CODE STARTS HERE //////////////////////
+            // TODO 7 : YOUR CODE STARTS HERE //////////////////////
             
+            // if the circle goes past the LEFT side place it on the RIGHT
+            if (circle.x < 0) {
+                circle.x = canvas.width;
+            }
+
+            // if the circle goes past the BOTTOM place it on the TOP
+            if (circle.y > canvas.height) {
+                circle.y = 0;
+            }
+
+            // if the circle goes past the TOP place it on the BOTTOM
+            if (circle.y < 0) {
+                circle.y = canvas.height;
+            }
 
 
-            // YOUR TODO 6 CODE ENDS HERE //////////////////////////
+            // YOUR TODO 7 CODE ENDS HERE //////////////////////////
         }
         
         /////////////////////////////////////////////////////////////
